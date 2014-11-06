16288e31bc23e8cca384c391edeaff8fdd1a3ef2
Matthew Simo <matthew.a.simo@gmail.com>
2014-11-06 06:59:43 -0600

    Refactor route declarations. change the header/footer includes into yield calls to handle them on a route-by-route basis. add workWithMe template

--- client/app.js
+++ client/app.js
@@ -3,15 +3,25 @@ Router.configure({
   layoutTemplate: 'layout',
   loadingTempalte: 'loading',
   notFoundTemplate: 'notFound',
-});
-
-Router.map(function () {
-
-  this.route('home', {
-    path: '/',
-  });
-
+	yieldRegions: {
+		'header': { to: 'header' },
+		'footer': { to: 'footer' }
+	},
 });
 
 
+Router.route('/', function () {
+	this.render('home');
+
+	this.render( 'homeHeader', { to: 'header' } );
+	this.render( 'homeFooter', { to: 'footer' } );
+});
+
+Router.route('/files/:filename', function () {
+	this.response.end("Loading file content\n");
+}, { where: 'server' });
+
+Router.route('/work-with-me', function () {
+	this.render('workWithMe');
+});
 
