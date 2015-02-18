eef5ebda4c13914e1d06d6b51dc71e6cc87b6faf
Matthew Simo <matthew.a.simo@gmail.com>
2015-02-17 21:58:47 -0600

    Update router endpoints

--- client/app.js
+++ client/app.js
@@ -10,14 +10,19 @@ Router.route('/', function () {
 	this.render('home');
 
 	this.render( 'homeHeader', { to: 'header' } );
-	this.render( 'homeFooter', { to: 'footer' } );
+//	this.render( 'homeFooter', { to: 'footer' } );
+	this.render( 'footer', { to: 'footer' } );
 });
 
 Router.route('/files/:filename', function () {
 	this.response.end("Loading file content\n");
 }, { where: 'server' });
 
-Router.route('/resume', function () {
+Router.route('resume', function() {
+	this.redirect('/work-with-me');
+});
+
+Router.route('/work-with-me', function () {
 	this.render('resume');
 
 	this.render( 'header', { to: 'header' } );
@@ -25,9 +30,9 @@ Router.route('/resume', function () {
 });
 
 
-Router.route('/portfolio', function() {
-	this.render('portfolio');
-
-	this.render( 'header', { to: 'header' } );
-	this.render( 'footer', { to: 'footer' } );
-});
+//Router.route('/portfolio', function() {
+//	this.render('portfolio');
+//
+//	this.render( 'header', { to: 'header' } );
+//	this.render( 'footer', { to: 'footer' } );
+//});
