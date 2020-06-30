abfaaee6cee1e96f9419903d6cbf6cb159fb7401
Matthew Simo <matthew.a.simo@gmail.com>
2020-06-29 20:33:35 -0500

    init next proj with theme-ui

---
+++ src/theme.js
@@ -0,0 +1,172 @@
+const nothing = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fonts: {
+    body:
+      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: "inherit",
+    monospace: "Menlo, monospace",
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  colors: {
+    text: "#000",
+    background: "#fff",
+    primary: "#07c",
+    secondary: "#30c",
+    muted: "#f6f6f6",
+  },
+  styles: {
+    root: {
+      fontFamily: "body",
+      lineHeight: "body",
+      fontWeight: "body",
+    },
+    h1: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 5,
+    },
+    h2: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 4,
+    },
+    h3: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 3,
+    },
+    h4: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 2,
+    },
+    h5: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 1,
+    },
+    h6: {
+      color: "text",
+      fontFamily: "heading",
+      lineHeight: "heading",
+      fontWeight: "heading",
+      fontSize: 0,
+    },
+    p: {
+      color: "text",
+      fontFamily: "body",
+      fontWeight: "body",
+      lineHeight: "body",
+    },
+    a: {
+      color: "primary",
+    },
+    pre: {
+      fontFamily: "monospace",
+      overflowX: "auto",
+      code: {
+        color: "inherit",
+      },
+    },
+    code: {
+      fontFamily: "monospace",
+      fontSize: "inherit",
+    },
+    table: {
+      width: "100%",
+      borderCollapse: "separate",
+      borderSpacing: 0,
+    },
+    th: {
+      textAlign: "left",
+      borderBottomStyle: "solid",
+    },
+    td: {
+      textAlign: "left",
+      borderBottomStyle: "solid",
+    },
+    img: {
+      maxWidth: "100%",
+    },
+  },
+};
+
+export default {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  initialColorModeName: "light",
+  useColorSchemeMediaQuery: true,
+  colors: {
+    text: "#000",
+    background: "#fff",
+    primary: "#07c",
+    secondary: "#609",
+    modes: {
+      dark: {
+        text: "#fff",
+        background: "#222",
+        primary: "#0cf",
+        secondary: "#90c",
+      },
+    },
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  fonts: {
+    body: "'Inter', sans-serif",
+    heading: "inherit",
+    variable: "'Inter var', sans-serif",
+    monospace: "Menlo, monospace",
+  },
+  styles: {
+    root: {
+      "@import url('https://rsms.me/inter/inter.css');": "",
+      "@supports (font-variation-settings: normal)": {
+        fontFamily: "variable",
+        fontVariationSettings: "'wght' 600, 'slnt' -1.5",
+      },
+      fontFamily: "body",
+      letterSpacing: "-0.014em",
+      color: "text",
+      bg: "background",
+    },
+    h1: {
+      fontSize: [4, 5, 6],
+      color: "primary",
+    },
+    a: {
+      color: "primary",
+      textDecoration: "none",
+      ":hover": {
+        color: "secondary",
+        textDecoration: "underline",
+      },
+    },
+  },
+};
