e761679fe9b4438cbcc648a66dc74e471bdc0cfa
Matthew Simo <matthew.a.simo@gmail.com>
2022-10-03 14:45:42 -0500

    burn it all down 🔥

--- components/ImageAttribution.js
+++
@@ -1,9 +0,0 @@
-export default function ImageAttribution(props) {
-  const linkClasses = '';
-  //    'text-main-300 hover:text-main-200 focus:text-main-200 hover:underline focus:underline';
-  return (
-    <div className="pt-1 sm:pt-2 xl:pt-3 text-main-300 italic sm:text-lg max-w-prose mx-auto">
-      <small {...props} />
-    </div>
-  );
-}
