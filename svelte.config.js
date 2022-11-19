f0ad851793f3de863a79275d7fa992865be59c78
Matthew Simo <matthew.a.simo@gmail.com>
2022-11-19 15:30:38 -0600

    another

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -19,8 +20,8 @@ const config = {
 	kit: {
 		adapter: vercel(),
 		alias: {
-			$lib: 'src/lib',
-			src: 'src'
+			$lib: resolve('./src/lib'),
+			src: resolve('./src')
 		}
 	}
 };
