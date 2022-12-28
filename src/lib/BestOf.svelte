006e4da7d1744ff498971378007aa343132515c5
Matthew Simo <matthew.a.simo@gmail.com>
2022-12-28 06:16:23 -0600

    2022.51 weekcap

---
+++ src/lib/BestOf.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Heading from './Heading.svelte';
+	import Link from './Link.svelte';
+
+	export let link: string;
+	export let title: string;
+</script>
+
+<Heading level={6} as="h4">
+	<Link href={link}>{title}</Link>
+</Heading>
